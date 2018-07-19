058325e0a11afcae8dbaa8adc731953bfca37bc1
sangnguyen7 <ngminhsang92@gmail.com>
2018-07-18 22:39:23 -0500

    update server.rb"

--- server.rb
+++ server.rb
@@ -2,7 +2,31 @@ require 'sinatra'
 require 'net/http'
 require 'ruby_query'
 
+class Opinion
+	
+	def setTitle(title)
+		@title = title
+	end
+	
+	def setYes(yes_p)
+		@yes = yes_p
+	end
+	
+	def setNo(no_p)
+		@no = no_p
+	end
+	
+	def to_s
+		print ('Title: ' + @title + '\n' +  @yes + ' \n' + @no)
+	end
+end
 get '/' do
 	res = Net::HTTP.get_response(URI(params['url']))
-	RubyQuery::Query.query(res.body, 'span.yes-text', 'text');
+	
+	op = Opinion.new()
+	op.setYes(RubyQuery::Query.query(res.body, 'span.yes-text', 'text'))
+	op.setNo(RubyQuery::Query.query(res.body, 'span.no-text', 'text'))
+	op.setTitle(RubyQuery::Query.query(res.body, 'div#col-wi span.q-title', 'text'))
+	op.to_s
+	
 end
