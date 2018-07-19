764d49e059c55a22a4d3986a51288f265c37aa1d
sangnguyen7 <ngminhsang92@gmail.com>
2018-07-19 00:01:43 -0500

    update server.rb using nokogiri

--- server.rb
+++ server.rb
@@ -1,8 +1,32 @@
 require 'sinatra'
 require 'net/http'
 require 'ruby_query'
+require 'nokogiri'
+require 'open-uri'
+
+class Vote
+	def initialize(mainPoint, comment, postedBy, like)
+		@mainPoint = mainPoint
+		@comment = comment
+		@postedBy = postedBy
+		@like = like
+	end
+	
+	def to_json
+		data = '{
+			"mainPoint": "#{@mainPoint}"
+		}'
+	end
+end
 
 class Opinion
+
+	def initialize(title, askedBy, yes, no)
+		@title = title
+		@askedBy = askedBy
+		@yes = yes
+		@no = no
+	end
 	
 	def setTitle(title)
 		@title = title
@@ -17,16 +41,25 @@ class Opinion
 	end
 	
 	def to_s
-		print ('Title: ' + @title + '\n' +  @yes + ' \n' + @no)
+		data = '{
+			"title": "' + @title + '",
+			"askedBy": "' + @askedBy + '",
+			"yes": "' + @yes + '",
+			"no": "' + @no + '"
+		}'
 	end
 end
 get '/' do
-	res = Net::HTTP.get_response(URI(params['url']))
+	doc = Nokogiri::HTML(open(params['url']))
+	#res = Net::HTTP.get_response(URI(params['url']))
 	
-	op = Opinion.new()
-	op.setYes(RubyQuery::Query.query(res.body, 'span.yes-text', 'text'))
-	op.setNo(RubyQuery::Query.query(res.body, 'span.no-text', 'text'))
-	op.setTitle(RubyQuery::Query.query(res.body, 'div#col-wi span.q-title', 'text'))
+	title = doc.css('div#col-wi span.q-title')#RubyQuery::Query.query(res.body, 'div#col-wi span.q-title', 'text')
+	#askedBy =  RubyQuery::Query.query(res.body, 'div#col-wi div.r-contain div.tags>a', 'text')
+	#yes = RubyQuery::Query.query(res.body, 'span.yes-text', 'text')
+	#no = RubyQuery::Query.query(res.body, 'span.no-text', 'text')
+	op = Opinion.new(title, "", "", "")
 	op.to_s
+	
+	#RubyQuery::Query.query(res.body, 'div#yes-arguments>ul>li>p', 'text')
 	
 end
